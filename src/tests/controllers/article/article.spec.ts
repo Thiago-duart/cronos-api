b1f03ff09990c6fff57b7bd324cd2ace23c1140d
ThiagoDuart <td1405150+github@gmail.com>
2024-02-06 09:45:50 -0300

    test: should return 400 and an error message = article not found

--- src/tests/controllers/article/article.spec.ts
+++ src/tests/controllers/article/article.spec.ts
@@ -1,6 +1,7 @@
 import { ArticleController } from "@/controllers/article/article";
 import { articleData } from "../mocks/article-data";
 import { IControllers } from "@/controllers/protocols";
+import { ParamError } from "@/controllers/errors/params-errors";
 
 describe("./src/controllers/article", () => {
   interface IMakeSut {
@@ -14,7 +15,15 @@ describe("./src/controllers/article", () => {
     const { article } = makeSut();
     const httpResponse = await article.handle(articleData.withoutTitle);
     expect(httpResponse).toEqual({
-      body: new Error("Title not found"),
+      body: new ParamError("title not found"),
+      statusCode: 400,
+    });
+  });
+  test("should return 400 and an error message = article not found", async () => {
+    const { article } = makeSut();
+    const httpResponse = await article.handle(articleData.withoutArticle);
+    expect(httpResponse).toEqual({
+      body: new ParamError("article not found"),
       statusCode: 400,
     });
   });
