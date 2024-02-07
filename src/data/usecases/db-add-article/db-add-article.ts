9538bd437681b7274c32d6fe086f6cfdc7957ab3
ThiagoDuart <td1405150+github@gmail.com>
2024-02-07 10:43:57 -0300

    feat: create db-add-article and call article repositore

---
+++ src/data/usecases/db-add-article/db-add-article.ts
@@ -0,0 +1,14 @@
+import { IArticleRepositore } from "@/data/interface/article-repositore";
+import { IArticle } from "@/domain/models/article";
+import { IAddArticle, IArticleRequest } from "@/domain/usecase/add-article";
+
+export class DbAddArticle implements IAddArticle {
+  private readonly articleRepositore: IArticleRepositore;
+  constructor(articleRepositore: IArticleRepositore) {
+    this.articleRepositore = articleRepositore;
+  }
+  async add(article: IArticleRequest): Promise<IArticle> {
+    const response = await this.articleRepositore.add(article);
+    return response;
+  }
+}
