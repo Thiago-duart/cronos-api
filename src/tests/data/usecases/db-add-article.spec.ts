f9060158df0ad7276bbf8342061cc665deaffde4
ThiagoDuart <td1405150+github@gmail.com>
2024-02-07 10:44:37 -0300

    test: should call corretly and return 201 in case of success

---
+++ src/tests/data/usecases/db-add-article.spec.ts
@@ -0,0 +1,41 @@
+import {
+  IArticleRepositore,
+  IArticleRequest,
+} from "@/data/interface/article-repositore";
+import { DbAddArticle } from "@/data/usecases/db-add-article/db-add-article";
+import { IArticle } from "@/domain/models/article";
+import { IAddArticle } from "@/domain/usecase/add-article";
+import { articleData } from "../../mocks/article-data";
+
+describe("./src/data/db-add-article", () => {
+  interface IMakeSut {
+    dbAddArticle: IAddArticle;
+    articleRepositoreStub: IArticleRepositore;
+  }
+  function makeSut(): IMakeSut {
+    class ArticleRepositoreStub implements IArticleRepositore {
+      async add(data: IArticleRequest): Promise<IArticle> {
+        const fakeArticle = {
+          id: "valid-id",
+          title: "valid-title",
+          article: "valid-article",
+        };
+        return fakeArticle;
+      }
+    }
+    const articleRepositoreStub = new ArticleRepositoreStub();
+    const dbAddArticle = new DbAddArticle(articleRepositoreStub);
+    return { dbAddArticle, articleRepositoreStub };
+  }
+  test("should call articleRepositore with corretly data", async () => {
+    const { dbAddArticle, articleRepositoreStub } = makeSut();
+    const addSpy = jest.spyOn(articleRepositoreStub, "add");
+    await dbAddArticle.add(articleData.validData.body);
+    expect(addSpy).toHaveBeenCalledWith(articleData.validData.body);
+  });
+  test("should return 201 in case of success", async () => {
+    const { dbAddArticle } = makeSut();
+    const response = await dbAddArticle.add(articleData.validData.body);
+    expect(response).toEqual(articleData.successArticle.body);
+  });
+});
