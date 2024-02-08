42b6bdac99a2a17cdd70cf85e665dd564d10a7bc
ThiagoDuart <td1405150+github@gmail.com>
2024-02-08 14:51:46 -0300

    feat: should call findIdArticle with corretly data

---
+++ src/controllers/article/findId-article.ts
@@ -0,0 +1,24 @@
+import { IFindIdArticle } from "@/domain/usecase/article-usecases";
+import { ServerError } from "../errors/server-error";
+import { badRequest, ok, serverError } from "../helpers/http-response";
+import { IControllers, IHttpRequest, IHttpResponse } from "../interface";
+import { ParamError } from "../errors/params-errors";
+
+export class FindIdArticleController implements IControllers {
+  private readonly findIdArticle: IFindIdArticle;
+  constructor(findIdArticle: IFindIdArticle) {
+    this.findIdArticle = findIdArticle;
+  }
+  async handle(httresquest?: IHttpRequest): Promise<IHttpResponse> {
+    try {
+      const { id } = httresquest?.body;
+      if (!id) {
+        return badRequest(new ParamError("id"));
+      }
+      const response = await this.findIdArticle.findId(id);
+      return ok(response);
+    } catch (error) {
+      return serverError(new ServerError(error.stack));
+    }
+  }
+}
