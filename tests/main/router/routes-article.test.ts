1640c2c05d855a065377737e97a1f01d4e7a556f
Thiago-duart <td1405150+github@gmail.com>
2024-02-16 19:05:52 -0300

    test: add a middleware to check ids exist

---
+++ tests/main/router/routes-article.test.ts
@@ -0,0 +1,34 @@
+import { mongoHelper } from "@/infra/db/mongodb/helpers/mongo-helper";
+import app from "@/main/app/app";
+import { articleData } from "../../../tests/mocks/article-data";
+
+import request from "supertest";
+describe("article", () => {
+    beforeAll(async () => {
+        await mongoHelper.connect(global.__MONGO_URI__);
+    });
+    beforeEach(async () => {
+        const collections = await mongoHelper.getCollection("articles");
+        await collections.deleteMany({});
+    });
+    afterAll(async () => {
+        await mongoHelper.desconect();
+    });
+    test("should must create an account in the database -- article", async () => {
+        await request(app)
+            .post("/api/article")
+            .send(articleData.validData.body)
+            .expect(function (res) {
+                res.body.id = "valid-id"
+                res.body.img = "valid-img"
+                res.body.title = "valid-title"
+                res.body.article = "valid-article"
+            }).expect(201, {
+                id: "valid-id",
+                img: "valid-img",
+                title: "valid-title",
+                article: "valid-article"
+            },)
+    });
+
+});
