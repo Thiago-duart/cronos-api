6311fb643c1b092352964409f8f43534bb64ab82
Thiago-duart <td1405150+github@gmail.com>
2024-02-16 21:42:31 -0300

    refactor: fixes bug id not valid

--- src/controllers/middlewares/articleId-exists.middleware.ts
+++ src/controllers/middlewares/articleId-exists.middleware.ts
@@ -2,6 +2,7 @@ import { IArticleRepositore } from "@/data/interface/article.repositore";
 import { IMiddleware } from "../interface/middlewares";
 import { IHttpResponse } from "../interface";
 import { notFound, ok } from "../helpers/http-response";
+import { ObjectId } from "mongodb";
 
 export class ArticleIdExistsMiddleware implements IMiddleware {
     private readonly articleRepositore: IArticleRepositore
@@ -10,10 +11,13 @@ export class ArticleIdExistsMiddleware implements IMiddleware {
     }
     async handle(request: any): Promise<IHttpResponse> {
         const id = request?.id || false
-        const article = await this.articleRepositore.findId(id)
-        if (!article) {
-            return notFound({ id: "id not found" })
+        if (ObjectId.isValid(id)) {
+            const article = await this.articleRepositore.findId(id)
+            if (!article) {
+                return notFound({ error: { id: "not found" } })
+            }
+            return ok("next")
         }
-        return ok("next")
+        return notFound({ error: { id: "not found" } })
     }
 }
